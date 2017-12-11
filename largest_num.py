9255175167a59cfbbb5b772d425d965c240f4384
sathyakrishna98 <34363536+sathyakrishna98@users.noreply.github.com>
2017-12-11 21:45:47 +0530

    Update largest_num.py

--- largest_num.py
+++ largest_num.py
@@ -2,8 +2,8 @@ num1=int(input())
 num2=int(input())
 num3=int(input())
 if (num1>num2) and (num1>num3):
-print(num1)
-else if(num2>num3):
-print(num2)
+  print(num1)
+elif(num2>num3):
+  print(num2)
 else:
-print(num3)
+  print(num3)
