eef80c60d1559268a81234bbb9dd220165252485
sathyakrishna98 <34363536+sathyakrishna98@users.noreply.github.com>
2017-12-12 07:52:37 +0530

    Update and rename sum1.py to sum1.c

---
+++ sum1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+  int inp,i,sum=0;
+  scanf("%d",&inp);
+  for(i=1;i<=inp;i++)
+  {
+    sum=sum+i;
+  }
+  printf("%d",sum);
+}
--- sum1.py
+++
@@ -1,6 +0,0 @@
-inp=int(input())
-sum=0
-i=1
-for i in inp:
-  sum=sum+i
-print(sum)
